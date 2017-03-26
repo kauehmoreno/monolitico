c88d5ad961e44ae0274f9e32a4c520aa24288209
Kaueh Moreno <kaueh.rodrigues@corp.globo.com>
2017-03-26 15:55:49 -0300

    Subindo estrutura básica do projeto - incluindo models.py

---
+++ materia/models.py
@@ -0,0 +1,25 @@
+# -#- coding:utf-8 -*-
+from __future__ import unicode_literals
+from django.db import models
+
+#TODO create signal to build slug automatically
+class Materia(models.Model):
+
+    titulo = models.CharField('Titulo da matéria', max_length=100, blank=False)
+    subtitulo = models.CharField('Subtitulo', max_length=255, blank=True)
+    cover = models.ImageField('Imagem Cover', upload_to='cover', blank=True, null=True)
+    data_criacao = models.DateTimeField('Criado em', auto_now_add=True)
+    data_modificacao = models.DateTimeField('Modificado em', auto_now=True)
+    data_publicacao = models.DateTimeField('Publicado em')
+    slug = models.SlugField('Permalink da matéria', max_length=255)
+
+    class Meta:
+        verbose_name = 'Matéria'
+        verbose_name_plural = 'Matérias'
+        ordering= ['data_publicacao']
+
+    def __str__(self):
+        return 'Materia: {}'.format(self.slug)
+
+    def permalink(self):
+        pass
