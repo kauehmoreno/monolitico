17513116de56b0cf16de358724cd2f802c5b9cda
Kaueh Moreno <kaueh.rodrigues@corp.globo.com>
2017-03-26 18:01:09 -0300

    Cria classe de integracao para buscar resultados no model

---
+++ materia/integration.py
@@ -0,0 +1,27 @@
+# -#- coding:utf-8 -*-
+from __future__ import unicode_literals
+from materia.models import Materia
+
+
+class  DbBridge(object):
+
+    @classmethod
+    def all(cls, resoure_class=None):
+        if resoure_class is None:
+            raise AttributeError(
+                    'Resource class must be provied otherwise it wont be '
+                    'possible to queryset Object'
+            )
+        materias = resoure_class.objects.all().order_by('-data_publicacao')
+        for materia in materias:
+            yield materia
+
+    @classmethod
+    def filter(cls, **kwargs):
+        slug = kwargs.get('slug')
+        resource_class = kwargs.get('resource_class')
+        try:
+            return resource_class.objects.get(slug=slug)
+        except Exception as e:
+            raise e
+
