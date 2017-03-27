19536a2c4ec31e3562f0146832d2e9b75f41e240
Kaueh Moreno <kaueh.rodrigues@corp.globo.com>
2017-03-27 14:45:38 -0300

    Cria view para renderizar materia única

--- materia/views.py
+++ materia/views.py
@@ -1,9 +1,10 @@
 # -- coding: utf-8 --
-
-from django.shortcuts import render
+import logging
 from django.views import generic
 from materia.integration import DbBridge
 from materia.models import Materia
+from django.http import Http404
+
 
 class MainPageView(generic.ListView):
 
@@ -16,4 +17,25 @@ class MainPageView(generic.ListView):
         return DbBridge.all(Materia)
 
 
+class SingleArticleView(generic.TemplateView):
 
+    template_name = 'materia/single_article.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(SingleArticleView, self).get_context_data(**kwargs)
+        try:
+            context['materia'] = DbBridge.filter(
+                slug=kwargs.get('slug'),
+                resource_class=Materia,
+                uuid=kwargs.get('uuid')
+            )
+            return context
+        except Materia.DoesNotExist as e:
+            logging.error(
+                'Materia não encontrada - {} - uuid: {} slug:{}'.format(
+                    e,
+                    kwargs.get('uuid'),
+                    kwargs.get('slug')
+                )
+            )
+            raise Http404('Não foi possível encontrar está matéria')
