1784cf41b746791edb780e07f5a6e7f833b916b5
Kaueh Moreno <kaueh.rodrigues@corp.globo.com>
2017-03-27 09:47:11 -0300

    alter table name

--- monolitico/settings.py
+++ monolitico/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'monolitico.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'materia',
+        'NAME': 'monolitico',
         'USER': 'root',
         'PASSWORD':'',
         'HOST': '127.0.0.1',
