7e2c3c3ea184386a8a78344fe90e2f43261f84be
Kaueh Moreno <kaueh.rodrigues@corp.globo.com>
2017-03-27 14:42:39 -0300

    Cria url para materia unica

--- materia/urls.py
+++ materia/urls.py
@@ -1,7 +1,12 @@
 # -- coding: utf-8 --
 from django.conf.urls import url
-from .views import MainPageView
+from .views import MainPageView, SingleArticleView
 
 urlpatterns = [
     url(r'^$', MainPageView.as_view(), name='home'),
+    url(
+        r'materia/(?P<uuid>[^/]+)/(?P<slug>[\w-]+).html$',
+        SingleArticleView.as_view(),
+        name='single_article'
+    )
 ]
