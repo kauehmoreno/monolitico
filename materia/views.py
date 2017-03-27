b9676f2eb5b8f7b6128fb8fe79a78bea0d61f824
Kaueh Moreno <kaueh.rodrigues@corp.globo.com>
2017-03-27 10:58:07 -0300

    Cria classe base para renderizar lista principal - definir questao de generator nao ter lista

--- materia/views.py
+++ materia/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+# -- coding: utf-8 --
 
-# Create your views here.
+from django.shortcuts import render
+from django.views import generic
+from materia.integration import DbBridge
+from materia.models import Materia
+
+class MainPageView(generic.ListView):
+
+    template_name = 'materia/home.html'
+    context_object_name = 'materias'
+    #paginate_by = 20
+
+
+    def get_queryset(self):
+        return DbBridge.all(Materia)
+
+
+
