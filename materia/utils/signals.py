192f1d06559fb43a2ecfef46d0ad23e0f03cbec9
Kaueh Moreno <kaueh.rodrigues@corp.globo.com>
2017-03-30 15:53:14 -0300

    Cria method que ira notificar microsevico em caso de delecao

--- materia/utils/signals.py
+++ materia/utils/signals.py
@@ -5,16 +5,36 @@ import logging
 from django.dispatch import receiver
 from django.db.models.signals import post_save, post_delete
 from materia.models import Materia
+from django.conf import settings
 
 @receiver(post_save, sender=Materia)
 def materia_save_actions(sender, **kwargs):
     instance = kwargs.get('instance')
+    host = '{}/insert/'.format(settings.MICROSERVICO_HOST_INTEGRATION)
     try:
-        r = requests.post('http://localhost:8888/api/v1/insert/', data = instance.to_dict())
+        r = requests.post(
+            host,
+            data = instance.to_dict()
+        )
     except Exception as e:
         msg = 'Não foi possível enviar os dados à applicacao microservico {}'
         logging.error(msg.format(e))
 
 
 
+@receiver(post_delete, sender=Materia)
+def materia_delete_actions(sender, **kwargs):
+    instance = kwargs.get('instance')
+    host = '{}/delete/'.format(settings.MICROSERVICO_HOST_INTEGRATION)
+
+    uuid = str(instance._id)
+
+    try:
+        r = requests.post(
+            host,
+            data = json.dumps({'uuid': uuid})
+        )
+    except Exception as e:
+        msg = 'Ocorreu um erro ao enviar objeto à aplicacao microservico: {}'
+        logging.error(msg.format(e))
 
